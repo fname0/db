d84c3f008337a94d1f5ae5f50b56e8e1f3bcd115
fname0 <kmailfw@mail.ru>
2023-06-15 10:41:12 +0300

    c

--- api/bridge.php
+++ api/bridge.php
@@ -1,8 +1,5 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-require_once __DIR__ . "/vendor/autoload.php";
-class_alias("\RedBeanPHP\R", "\R");
-R::setup( 'mysql:host=95.174.102.106;dbname=kamaz', 'remote', 'yaDumalPass');
 $myCurl = curl_init();
 curl_setopt_array($myCurl, array(
     CURLOPT_URL => 'http://95.174.102.106/test.php',
--- api/test.php
+++ api/test.php
@@ -1,7 +1,4 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-require_once __DIR__ . "/vendor/autoload.php";
-class_alias("\RedBeanPHP\R", "\R");
-R::setup( 'mysql:host=95.174.102.106;dbname=kamaz', 'remote', 'yaDumalPass');
 file_put_contents("post.log", print_r($_POST, true));
 ?>
