631b3f3a7278aa7e675ef396f17b6796e233bc04
fname0 <kmailfw@mail.ru>
2023-06-26 10:43:52 +0300

    c

--- api/setproductinfo.php
+++ api/setproductinfo.php
@@ -11,8 +11,17 @@ $product->title = $data->title;
 $product->price = $data->price;
 $product->num = $data->num;
 R::store($product);
-error_log($_FILES['file']['name']);
-$img_name = "productImg/".$data->id.".png";
-$tmp_img_name=$_FILES['file']['tmp_name'];
-move_uploaded_file($tmp_img_name,$img_name);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://95.174.102.106/api/bridge.php");
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+    'file' => 'p',
+));
+$result = curl_exec($ch);
+curl_close($ch);
+// $img_name = "productImg/".$data->id.".png";
+// $tmp_img_name=$_FILES['file']['tmp_name'];
+// move_uploaded_file($tmp_img_name,$img_name);
 ?>
