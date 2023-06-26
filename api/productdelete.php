be5400523c336d982697fb77055206ef6a3535b6
fname0 <kmailfw@mail.ru>
2023-06-26 08:57:08 +0300

    c

--- api/productdelete.php
+++ api/productdelete.php
@@ -2,6 +2,6 @@
 header('Access-Control-Allow-Origin: *');
 require_once __DIR__ . "/vendor/autoload.php";
 class_alias("\RedBeanPHP\R", "\R");
-R::setup( 'mysql:host=db4free.net;dbname=kamazdb', 'remoteconn', 'yaDumalPass');
+R::setup( 'mysql:host=95.174.102.106;dbname=kamaz', 'remote', 'yaDumalPass');
 R::trash(R::findOne('products', "WHERE id = ?", [$_GET['id']]));
 ?>
