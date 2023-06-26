dc3a6907fb63ad2f14ade143f1faa255b13685f2
fname0 <kmailfw@mail.ru>
2023-06-26 11:27:03 +0300

    с

---
+++ api/bridge.php
@@ -0,0 +1,5 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+$img_name = "productImg/".$data->id.".png";
+$tmp_img_name=$_FILES['file']['tmp_name'];
+move_uploaded_file($tmp_img_name,$img_name);
