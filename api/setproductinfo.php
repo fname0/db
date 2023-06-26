4aa08fe5a1a64f8c9fb918771c86fb9709a59265
fname0 <kmailfw@mail.ru>
2023-06-26 12:10:36 +0300

    c

--- api/setproductinfo.php
+++ api/setproductinfo.php
@@ -11,14 +11,16 @@ $product->title = $data->title;
 $product->price = $data->price;
 $product->num = $data->num;
 R::store($product);
+if (function_exists('curl_file_create')) {
+    $cFile = curl_file_create($_FILES['file']['tmp_name']);
+} else {
+    $cFile = '@' . realpath($_FILES['file']['tmp_name']);
+}
+$post = array('id' => $data->id,'file'=> $cFile);
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://95.174.102.106:7474/api/bridge.php");
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, array(
-    'file' => $_FILES['file']['tmp_name'],
-));
-$result = curl_exec($ch);
-curl_close($ch);
+curl_setopt($ch, CURLOPT_URL,'http://95.174.102.106:7474/api/bridge.php');
+curl_setopt($ch, CURLOPT_POST,1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+$result=curl_exec ($ch);
+curl_close ($ch);
 ?>
