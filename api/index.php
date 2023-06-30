77bf2e08569181c66afa17207023d97bbb687316
fname0 <kmailfw@mail.ru>
2023-06-30 22:12:10 +0300

    c

--- api/index.php
+++ api/index.php
@@ -3,5 +3,5 @@ header('Access-Control-Allow-Origin: *');
 require_once __DIR__ . "/vendor/autoload.php";
 class_alias("\RedBeanPHP\R", "\R");
 R::setup( 'mysql:host=95.174.102.106;dbname=kamaz', 'remote', 'yaDumalPass');
-echo json_encode(R::findAll("products", "WHERE cat = ? AND id > ? AND deleted = 0 LIMIT 32", [$_GET['cat'], $_GET['start']]), JSON_UNESCAPED_UNICODE);
+echo json_encode(R::findAll("products", "WHERE cat = ? AND id > ? AND deleted = 0 LIMIT 64", [$_GET['cat'], $_GET['start']]), JSON_UNESCAPED_UNICODE);
 ?>
